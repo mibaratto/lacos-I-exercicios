b11b3c8828238596c5effa83231c7eac78fc437c
Michelle Lang Baratto <mi.baratto@gmail.com>
2022-12-05 16:59:20 +0100

    Laços exercícios - todos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,9 +1,10 @@
-// let querCoxinha = prompt(`Você quer mais coxinha?`)
-// let conta = 0
+let querCoxinha = prompt(`Você quer coxinha?
+Digite "S" para sim e "N" para não`).toUpperCase()
 
-// while( querCoxinha === "S"){
-//     conta = conta + 2.50
-//     querCoxinha = prompt(`Você quer mais coxinha?`)
-// }
+let conta = 0
 
-// console.log(conta)
+while( querCoxinha === "S"){
+    conta = conta + 2.50
+    querCoxinha = prompt(`Você quer mais coxinha? \n Digite "S" para sim e "N" para não`).toUpperCase()
+}
+console.log(`Sua conta: ${conta}`)
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,5 +1,5 @@
 let numero = +prompt(`Digite um número:`)
-
+console.log(`Tabuada do ${numero}`)
 for(i = 0; i < 10; i ++ ){
     console.log(`${numero} x ${i + 1} = ${numero*(i+1)}`)
 }
